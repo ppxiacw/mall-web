d2283c4654ffa173170f64fcf6f46f6650989d5c
ppxiacw <970488001@qq.com>
2021-01-07 22:19:36 +0800

    '分类新增功能'

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <el-tree :props="props" :data="data"  show-checkbox @check-change="handleCheckChange"></el-tree>
+    <el-tree
+      :props="props"
+      node-key="catId"
+      :data="data"
+      show-checkbox
+      :default-expanded-keys="expandedKeys"
+      @check-change="handleCheckChange"
+      :expand-on-click-node="false"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button type="text" v-if="node.level!=3" size="mini" @click="() => showDialog(data)">Append</el-button>
+          <el-button
+            type="text"
+            v-if="node.childNodes.length == 0"
+            size="mini"
+            @click="() => remove(node, data)"
+          >Delete</el-button>
+        </span>
+      </span>
+    </el-tree>
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form :model="category">
+        <el-form-item label="活动名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="append">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -10,7 +42,10 @@
 export default {
   data() {
     return {
-        data:[],
+      data: [],
+      category: { name: "", parentId: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      expandedKeys: [],
+      dialogFormVisible:false,
       props: {
         label: "name",
         children: "children"
@@ -19,21 +54,74 @@ export default {
     };
   },
   created() {
-      this.getCategory()
+    this.getCategory();
   },
   methods: {
-    getCategory(){
-        this.$http({
-              url: this.$http.adornUrl(`/product/category/list/tree/`),
-              method: 'get',
-              params: this.$http.adornParams()
-            }).then(data=>{
-                {
-                this.data =data.data.data
-                console.log(data.length);
-                
-            }
-            })
+    showDialog(data){
+      console.log(data);
+      
+      this.dialogFormVisible = true
+         this.category.catLevel = +data.catLevel+1
+      this.category.parentCid = data.catId
+    },
+    append() {
+      if (this.category.name.trim()=='') {
+        this.$message.error('不能为空')
+        return
+      }
+      this.category.name = this.category.name
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false)
+      }).then(({ data }) => {
+       this.$message.success('添加chengg')
+       this.dialogFormVisible=false
+       this.getCategory()
+       this.expandedKeys = [this.category.parentCid];
+      });
+    },
+
+    remove(node, data) {
+      this.$confirm(`是否删除【${data.name}】？', '确认信息`, {
+        distinguishCancelAndClose: true,
+        confirmButtonText: "确定",
+        cancelButtonText: "取消"
+      })
+        .then(() => {
+          let ids = [data.catId];
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false)
+          }).then(({ data }) => {
+            this.$message({
+              type: "info",
+              message: "删除成功"
+            });
+            this.getCategory();
+            this.expandedKeys = [node.parent.data.catId];
+          });
+        })
+        .catch(action => {
+          this.$message({
+            type: "info",
+            message:
+              action === "cancel" ? "放弃保存并离开页面" : "停留在当前页面"
+          });
+        });
+    },
+    getCategory() {
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/list/tree/`),
+        method: "get",
+        params: this.$http.adornParams()
+      }).then(data => {
+        {
+          this.data = data.data.data;
+          console.log(data.length);
+        }
+      });
     },
     handleCheckChange(data, checked, indeterminate) {
       console.log(data, checked, indeterminate);
